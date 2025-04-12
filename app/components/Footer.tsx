efea1a810c5d34ec44a8a0038269ef91a21f4ad3
emerson000 <94085544+emerson000@users.noreply.github.com>
2025-04-12 11:18:01 -0400

    feat: add Footer component and integrate GitHub icon

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+import { SiGithub } from '@icons-pack/react-simple-icons';
+
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 py-4 dark:bg-black">
+      <div className="container mx-auto px-4 flex items-center justify-center gap-2">
+        <a href="https://github.com/emerson000/emerson.sh" className="underline" target="_blank" rel="noopener noreferrer">
+            <SiGithub />
+        </a>
+      </div>
+    </footer>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import { Header } from "./components/Header";
+import Footer from "./components/Footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -38,6 +39,7 @@ export default function RootLayout({
         >
           <Header />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>

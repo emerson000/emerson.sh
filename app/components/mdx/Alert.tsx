f07031d1484c32778e11217e67851eeb9b57f3f3
emerson000 <94085544+emerson000@users.noreply.github.com>
2025-05-04 18:37:11 -0400

    feat: alert component title

--- app/components/mdx/Alert.tsx
+++ app/components/mdx/Alert.tsx
@@ -2,10 +2,11 @@ import { ReactNode } from 'react';
 
 type AlertProps = {
   type?: 'info' | 'warning' | 'error' | 'success';
+  title?: string;
   children: ReactNode;
 };
 
-export function Alert({ type = 'info', children }: AlertProps) {
+export function Alert({ type = 'info', title, children }: AlertProps) {
   const colors = {
     info: 'bg-blue-50 text-blue-800 border-blue-200',
     warning: 'bg-yellow-50 text-yellow-800 border-yellow-200',
@@ -14,7 +15,8 @@ export function Alert({ type = 'info', children }: AlertProps) {
   };
 
   return (
-    <div className={`p-4 mb-4 rounded-lg border ${colors[type]}`}>
+    <div className={`p-4 mt-0 mb-4 rounded-lg border ${colors[type]}`}>
+      {title && <b className="font-bold p-0 m-0">{type == 'warning' ? '⚠️' : type == 'error' ? '❌' : type == 'success' ? '✅' : 'ℹ️'} {title}</b>}
       {children}
     </div>
   );
